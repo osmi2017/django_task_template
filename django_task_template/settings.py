4885e9bb7df827e06024cdd75446c1ac3e892756
Soro Ismael <soroismael7@gmail.com>
2022-05-27 17:00:25 +0000

    editIpda

--- django_task_template/settings.py
+++ django_task_template/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "staticfiles
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
